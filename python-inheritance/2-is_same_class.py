b434e35e3ded88f887f8bce038c3419e93e0bd11
Tommy JOUHANS <12119@holbertonstudents.com>
2026-02-02 19:07:48 +0000

    is same class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Module that provides a function that returns:
+True/False if obj is a type of a_class.
+
+"""
+
+
 def is_same_class(obj, a_class):
     """ Function that returns True/False if obj is a type of a_class
 
