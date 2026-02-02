274ceaeadfeee5e4935edeb7fcd2de3e19cf902e
Tommy JOUHANS <12119@holbertonstudents.com>
2026-02-02 21:39:20 +0000

    duck typing

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+Import ABC (Abstract Base Class) and abstractmethod
+from the abc module, these allow you to create abstract classes.
+
+"""
+
+
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    """Classe abstraite représentant une forme géométrique."""
+
+    @abstractmethod
+    def area(self):
+        """Retourne l'aire de la forme."""
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """Retourne le périmètre de la forme."""
+        pass
+
+
+class Circle(Shape):
+    """Représente un cercle."""
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        """Calcule l'aire du cercle."""
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        """Calcule le périmètre du cercle."""
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    """Représente un rectangle."""
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """Calcule l'aire du rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Calcule le périmètre du rectangle."""
+        return 2 * (self.width + self.height)
+
+
+def shape_info(shape):
+    """Affiche l'aire et le périmètre d'une forme."""
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
