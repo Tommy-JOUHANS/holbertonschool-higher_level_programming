efae2dc44634e109986102bd5b5987e1376bd7e1
Tommy JOUHANS <12119@holbertonstudents.com>
2026-02-02 21:48:00 +0000

    duck typing

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-"""
-Import ABC (Abstract Base Class) and abstractmethod
-from the abc module, these allow you to create abstract classes.
-
-"""
-
-
 from abc import ABC, abstractmethod
 import math
 
