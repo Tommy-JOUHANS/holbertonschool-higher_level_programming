4478653aedafcaa6f695b5921ae502ba24dfc018
Tommy JOUHANS <12119@holbertonstudents.com>
2026-02-02 19:23:32 +0000

    inherits from

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Module that provides a function that returns:
+True/False if obj is an instance of a_class.
+
+"""
+
+
 def inherits_from(obj, a_class):
     """ Function that returns True/False if obj is an instance of a_class
 
