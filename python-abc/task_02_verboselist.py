998d1da1033a7cc2d0d249771606cc1b20780e65
Tommy JOUHANS <12119.holbertonstudients.com>
2026-02-03 19:02:55 +0000

    verboselist.py

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -5,9 +5,26 @@ from the abc module, these allow you to create abstract classes.
 
 """
 
-from abc import ABC, abstractmethod
-import math
 
-class VerboseList{
-    
-}
+class VerboseList(list):
+    # Override append to print a message after adding an item
+    def append(self, item):
+        super().append(item)  # Call the original list.append
+        print(f"Added [{item}] to the list")
+
+    # Override extend to print how many items were added
+    def extend(self, iterable):
+        count = len(iterable)  # Number of elements being added
+        super().extend(iterable)  # Call the original list.extend
+        print(f"Extended the list with [{count}] items")
+
+    # Override remove to print a message before removing the item
+    def remove(self, item):
+        print(f"Removed [{item}] from the list")
+        super().remove(item)  # Call the original list.remove
+
+    # Override pop to print a message before removing the item
+    def pop(self, index=-1):
+        item = self[index]  # Retrieve the element before removal
+        print(f"Popped [{item}] from the list")
+        return super().pop(index)  # Call the original list.pop
