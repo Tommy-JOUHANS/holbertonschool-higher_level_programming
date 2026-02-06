65c358997bed814e31db929108638b932194589f
Tommy JOUHANS <12119@holbertonstudents.com>
2026-02-06 11:13:26 +0000

    duck typping

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -10,20 +10,18 @@ import math
 
 
 class Shape(ABC):
-    """ Abstract base class representing a geometric shape.
-    """
+    """ Abstract base class representing a geometric shape."""
 
     @abstractmethod
     def area(self):
-        """ Return the area of the shape.
-        """
-        raise NotImplementedError("The area of the shape is not defined")
+        """ Return the area of the shape."""
+        pass
 
     @abstractmethod
     def perimeter(self):
         """Return the perimeter of the shape.
         """
-        raise NotADirectoryError("The area of the shape is not defined")
+        pass
 
 
 class Circle(Shape):
@@ -39,30 +37,27 @@ class Circle(Shape):
         return math.pi * (self.radius ** 2)
 
     def perimeter(self):
-        """ Compute the perimeter (circumference) of the circle.
-        """
+        """Compute the perimeter (circumference) of the circle."""
         return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
-    """ Class representing a rectangle.
-    """
+    """Class representing a rectangle."""
 
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
     def area(self):
-        """ Compute the area of the rectangle.
-        """
+        """Compute the area of the rectangle."""
         return self.width * self.height
 
     def perimeter(self):
-        """ Compute the perimeter of the rectangle.
-        """
+        """Compute the perimeter of the rectangle."""
         return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
+    """Print area and perimeter of shape"""
     print("Area:", shape.area())
     print("Perimeter:", shape.perimeter())
