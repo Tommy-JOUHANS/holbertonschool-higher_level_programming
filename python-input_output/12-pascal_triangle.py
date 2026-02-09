6efdbbf0dcd9e12b017e8ac25b5c9733afb6f6fe
Tommy JOUHANS <12119@holbertonstudents.com>
2026-02-09 16:40:36 +0100

    12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -4,6 +4,8 @@ that returns a list of lists of integers representing
  the Pascal’s triangle of n.
 
 """
+
+
 def pascal_triangle(n):
     """Function that returns a list of lists of integers representing
      the Pascal’s triangle of n.
