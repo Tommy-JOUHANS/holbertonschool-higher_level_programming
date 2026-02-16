3dfebdedab409f8e08025b0a2bcbadb93f91b8bd
Tommy JOUHANS <12119@holbertonstudents.com>
2026-02-17 00:10:12 +0100

    flask

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -9,20 +9,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-users = {
-    "jane": {
-    "username": "jane",
-    "name": "Jane",
-    "age": 28,
-    "city": "Los Angeles"
-   },
-"john": {
-    "username": "john",
-    "name": "John",
-    "age": 30,
-    "city": "New York"
-   }
-}
+users = {}
 
 
 @app.route("/", methods=["GET"])
@@ -78,4 +65,4 @@ def add_user():
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000)
+    app.run()
