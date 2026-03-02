75fe5defb6a4161b735239067f80653a85d71efd
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-02 15:38:44 +0100

    listes de la table

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- List all tables in the current database
+-- The `SHOW TABLES` statement is used to display a list of all tables in the current database
+-- show tables from mysql ;
+SHOW TABLES FROM mysql ;
