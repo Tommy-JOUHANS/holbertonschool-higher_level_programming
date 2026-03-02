bbe4289405b5ccb77ed319c63db40bb1dbd0c8c3
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-02 21:58:25 +0100

    2nd table

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,4 @@
+-- Creates a table named `second_table` in the current database with specific columns and constraints
+-- Insert a new row into the `second_table` with the following values
+CREATE TABLE second_table (id INT,name VARCHAR(256),score INT,);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
