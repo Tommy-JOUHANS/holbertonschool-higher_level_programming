d30593ba678212afb4cacf414d362b37cef5a38d
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-02 16:25:06 +0100

    first table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Create a table named `first_table` with the following columns:
+-- - `id`: an integer column to store the unique identifier for each record.
+-- - `name`: a variable character column with a maximum length of 255 characters to store the name associated with each record.
+CREATE TABLE first_table (
+    id INT,
+    name VARCHAR(255)
+);
