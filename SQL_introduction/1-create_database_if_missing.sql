cd646469ef609a63be61afae6731d9aa791f7396
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-02 14:23:17 +0100

    test create database

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- Create a database named `hbtn_0c_0` if it does not already exist
+-- The `CREATE DATABASE` statement is used to create a new database in the MySQL server.
+-- The `IF NOT EXISTS` clause is used to prevent an error from occurring if the database already exists.
+CREATE DATABASE IF NOT EXISTS hbtn_oc_0;
