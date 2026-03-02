7b793a5ff2adb4baa039fdd581a0c9c68a26253a
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-02 15:20:37 +0100

    delete

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,4 @@
 -- Create a database named `hbtn_0c_0` if it does not already exist
 -- The `CREATE DATABASE` statement is used to create a new database in the MySQL server.
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- The `IF NOT EXISTS` clause is used to prevent an error from occurring if the database already exists. 
+CREATE DATABASE hbtn_0c_0;
---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Delete database `hbtn_0c_0`
+-- The `DROP DATABASE` statement is used to delete an existing database in the MySQL server.
+DROP DATABASE IF EXISTS hbtn_0c_0;
