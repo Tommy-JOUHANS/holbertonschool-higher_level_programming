560b43bf6d69b50a60cf57044e8c1b99b9511597
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-03 22:27:01 +0100

    safe filter states

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,8 +4,7 @@ Lists all values in the stables tables of a database
 where name matches the argument
 """
 import sys
-import MySQLdb  
-
+import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
