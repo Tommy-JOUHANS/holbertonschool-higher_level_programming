39880948a4f2c5f5b32467797fe196b3bc7b8545
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-03 10:18:26 +0100

    privilege

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
-CREATE USER IF NOT EXISTS 'holberton_user'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'holberton_user'@'localhost';
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
