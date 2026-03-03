f8fb55063de0eb974610cd8bd6b7bbd5db62d11d
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-03 11:47:27 +0100

    test

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Script that lists all privileges of the MySQL users
--- Query to list all privileges (GRANT) of the MySQL users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Query to list all privileges (GRANT) of the MySQL users
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
