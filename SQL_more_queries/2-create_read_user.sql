993abe5d90037954b071f15ffcb97224b04b8c1f
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-03 10:59:07 +0100

    Read user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- Create the database named hbtn__0d2 and the user user_0d_2 with password user_0d_2_pwd
+-- User_0d_2 should have all SELECT privileges on the database hbtn__0d2
+-- The user_0d_2 password should be set to user_0d_2_pwd
+-- If the database hbtn__0d2 already exists, your script should not fail
+-- If the user user_0d_2 already exists, your script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
