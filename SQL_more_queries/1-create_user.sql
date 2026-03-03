89a042186064630bff1c4bb2c0dec801c24c0eb2
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-03 10:33:34 +0100

    root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,7 @@
 -- Creates a MySQL user named user_0d_1 with password user_0d_1_pwd and grants all privileges on the database database_0d_1 to this user.
+-- User_0d_1 should have all privileges on your MySQL server
+-- The user_0d_1 password should be set to user_0d_1_pwd
+-- If the user user_0d_1 already exists, your script should not fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON `database_0d_1`. * TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON database_0d_1.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
