d03d6e27125d1f3eb2b63f340e5f3bd39e3914a6
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-03 16:44:01 +0100

    count shows by genre

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Import the database dump of hbtn_0d_tvshows to your MySQL server
+-- Script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each genre
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows -- Query to join tv_show_genres and count the number of shows linked to each genre
+FROM tv_show_genres
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
