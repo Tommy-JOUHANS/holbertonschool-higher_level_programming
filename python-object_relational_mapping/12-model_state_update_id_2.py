2d1a088635610bb8d2f3bf98bfa171a36445d57e
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-05 12:10:29 +0100

    model state update

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that changes the name of the State object with id = 2
+Script that changes the name of the State object with id = 2
 to 'New Mexico' in the database hbtn_0e_6_usa
 """
 
@@ -12,18 +12,31 @@ from model_state import Base, State
 
 def update_state():
     """Update the name of State with id=2 to 'New Mexico'"""
-    arg = sys.argv
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>".
+              format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(arg[1], arg[2], arg[3]),
+        'mysql+mysqldb://{}:{}@localhost/{}'.
+        format(username, password, database),
         pool_pre_ping=True
     )
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Récupérer l'état avec id=2
+    # Retrieve the state with id=2
     state = session.query(State).get(2)
     if state:
         state.name = "New Mexico"
         session.commit()
 
     session.close()
+
+
+if __name__ == "main":
+    update_state()
