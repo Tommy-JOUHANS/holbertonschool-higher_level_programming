930d6288f0074129d12cb2296f78a6e097660156
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-05 12:37:49 +0100

    go to utf8

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
--- Lists all scores and the number of records for each score in the table second_table of the database hbtn_0c_0
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
+-- Lists all records of the table second_table of the database hbtn_0c_0
+SELECT score, name FROM second_table;
