99fbe760e19b7e5b4f0876f531afe6132c282911
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-05 22:05:00 +0100

    not my genres

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,7 +1,16 @@
 -- Uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
-SELECT DISTINCT tv_genres.name
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE NOT tv_shows.title = "Dexter"
+SELECT tv_genres.name -- Query to get genres that not belong
+FROM tv_genres
+LEFT JOIN
+(
+	SELECT tv_genres.id, tv_genres.name -- Query to get Dexter genres
+	FROM tv_genres
+	JOIN tv_show_genres
+	     ON tv_genres.id = tv_show_genres.genre_id
+	JOIN tv_shows
+	     ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_shows.title = "Dexter"
+	ORDER BY tv_genres.id
+) dexter_genres ON dexter_genres.id = tv_genres.id
+WHERE dexter_genres.id is NULL
 ORDER BY tv_genres.name;
