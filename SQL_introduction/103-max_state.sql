ae454da6f90605cb489004884ed622eeffed07e1
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-05 17:11:53 +0100

    max state

--- SQL_introduction/103-max_state.sql
+++ SQL_introduction/103-max_state.sql
@@ -1,7 +1,6 @@
 -- Displays the max temperature of each state
 SELECT state, MAX(value) AS max_temp
 FROM temperatures
-WHERE month IN (7, 8)
 GROUP BY state
 ORDER BY max_temp DESC, state ASC
 LIMIT 3;
