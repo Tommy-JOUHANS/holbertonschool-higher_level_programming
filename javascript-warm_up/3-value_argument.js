6703c770d5ed19f506b77fee50c8e6949d5d8151
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-18 10:13:19 +0100

    value arguments

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
