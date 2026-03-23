6cf4e124b7ee3307087ee53091da5bd59a060f84
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-23 13:59:58 +0100

    Change the text

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+document.getElementById('update_header').addEventListener('click', function () {
+  const header = document.querySelector('header');
+  header.textContent = 'New Header!!!';
+});
