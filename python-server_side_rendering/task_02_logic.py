ee885b166f5c25130ac23f3b9a1138e970f28e4d
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-24 13:25:19 +0100

    Creating a Dynamic Template with Loops and Conditions in Flask

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -21,7 +21,7 @@ def contact():
 def items():
     with open('items.json', 'r') as f:
         items_data = json.load(f)
-    return render_template('items.html', items=items_data['items'])
+    return render_template('items.html', items=items_data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
