697abf337f691be372a29b960e02f40f3c682621
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-30 19:39:33 +0200

    105 line1

--- python-everything_is_object/int.py
+++ python-everything_is_object/int.py
@@ -1,8 +1,3 @@
-a = 1024
-b = 1024
-del a
-del b
-c = 1024
-print(c is a)  # False — they point to DIFFERENT objects
-print(id(c) == id(a))  # False
-
+print(int("1"))
+print(int("2"))
+print(int("3"))
